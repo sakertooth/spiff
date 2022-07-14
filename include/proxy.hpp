fbe54bb786475ed5a7f41ab71066fb863d63e1c8
sakertooth <sakertooth@gmail.com>
2022-07-14 10:12:46 -0400

    Wrap up TCP proxy

--- include/proxy.hpp
+++ include/proxy.hpp
@@ -12,21 +12,26 @@ namespace spiff {
 
         void start(std::string_view server_endpoint);
         void start_accept();
-        void handle_connect(const asio::error_code& ec, const asio::ip::tcp::endpoint& endpoint);
         void handle_accept(const asio::error_code& ec, asio::ip::tcp::socket peer);
 
     private:
         asio::io_context m_io;
-        asio::ip::tcp::socket m_server_socket {m_io};
         asio::ip::tcp::acceptor m_proxy_server;
+        asio::ip::tcp::endpoint m_server_endpoint;
         int m_protocol_version;
     };
 
     class minecraft_proxy_connection : public std::enable_shared_from_this<minecraft_proxy_connection> {
     public:
-        minecraft_proxy_connection(asio::ip::tcp::socket&& socket);
+        minecraft_proxy_connection(asio::io_context& io, asio::ip::tcp::socket&& client_to_proxy_socket, asio::ip::tcp::endpoint m_server_endpoint);
         void start();
+        void handle_connect(const asio::error_code& ec);
+        void handle_read(asio::error_code& ec, size_t bytes_transferred);
+        void start_transmission();
     private:
-        asio::ip::tcp::socket m_socket;
+        asio::ip::tcp::socket m_client_to_proxy_socket;
+        asio::ip::tcp::socket m_proxy_to_server_socket;
+        asio::ip::tcp::endpoint m_server_endpoint;
+        asio::mutable_buffer m_read_buffer;
     };
 };
--- src/proxy.cpp
+++ src/proxy.cpp
@@ -1,6 +1,8 @@
 #include "proxy.hpp"
 #include "asio/ip/tcp.hpp"
 #include "asio/connect.hpp"
+#include "asio/read.hpp"
+#include "asio/write.hpp"
 
 #include <functional>
 #include <iostream>
@@ -13,23 +15,24 @@ namespace spiff {
 
     void minecraft_proxy::start(std::string_view server_endpoint) {
         const auto colon_pos = server_endpoint.find(':', 0);
-        const auto& server_address = colon_pos != std::string_view::npos ? server_endpoint.substr(0, colon_pos) : server_endpoint;
+        const auto server_address = colon_pos != std::string_view::npos ? server_endpoint.substr(0, colon_pos) : server_endpoint;
         const auto server_port = colon_pos != std::string_view::npos ? server_endpoint.substr(colon_pos + 1) : "25565";
 
         asio::ip::tcp::resolver resolver{m_io};
-        asio::async_connect(m_server_socket, resolver.resolve(server_address, server_port), std::bind(&minecraft_proxy::handle_connect, this, std::placeholders::_1, std::placeholders::_2));
-        m_io.run();
-    }
+        auto resolved = resolver.resolve(server_address, server_port);
 
-    void minecraft_proxy::handle_connect(const asio::error_code& ec, const asio::ip::tcp::endpoint& endpoint) { 
-        if (!ec) {
-            std::cout << "Proxy connection made to " << endpoint << '\n';
-            std::cout << "Listening on " << m_proxy_server.local_endpoint() << "..." << '\n';
-            start_accept();
+        if (resolved.size() == 0) {
+            std::cerr << "Failed to resolve hostname: " << server_endpoint << '\n';
+            return;
         }
-        else {
-            throw std::runtime_error{"Failed to resolve and connect to server: " + ec.message()};
-        }
+
+        m_server_endpoint = *resolved.begin();
+
+        std::cout << "Resolved hostname to " << m_server_endpoint << '\n';
+        std::cout << "Listening on " << m_proxy_server.local_endpoint() << "...\n";
+        start_accept();
+
+        m_io.run();
     }
 
     void minecraft_proxy::start_accept() {
@@ -38,20 +41,31 @@ namespace spiff {
 
     void minecraft_proxy::handle_accept(const asio::error_code& ec, asio::ip::tcp::socket peer) {
         if (!ec) {
-            //TODO
-            std::cout << "Connection: " << peer.remote_endpoint() << " <-> Proxy <-> " << m_server_socket.remote_endpoint() << '\n';
-            std::shared_ptr<minecraft_proxy_connection> conn = minecraft_proxy_connection{std::move(peer)}.shared_from_this();
+            std::cout << "Connection made to proxy: " << peer.remote_endpoint() << '\n';
+            auto conn = std::make_shared<minecraft_proxy_connection>(m_io, std::move(peer), m_server_endpoint);
             conn->start();
-            start_accept();
         }
-        else {
-            std::cout << "Accepting new connection failed: " << ec.message() << '\n';
-        }
+
+        start_accept();
     }
 
-    minecraft_proxy_connection::minecraft_proxy_connection(asio::ip::tcp::socket&& socket) : m_socket(std::move(socket)) {}
+    minecraft_proxy_connection::minecraft_proxy_connection(asio::io_context& io, asio::ip::tcp::socket&& client_to_proxy_socket, asio::ip::tcp::endpoint server_endpoint) 
+        : m_client_to_proxy_socket(std::move(client_to_proxy_socket)),
+          m_proxy_to_server_socket(io),
+          m_server_endpoint(server_endpoint) {}
 
     void minecraft_proxy_connection::start() {
-
+        m_proxy_to_server_socket.async_connect(m_server_endpoint, std::bind(&minecraft_proxy_connection::handle_connect, shared_from_this(), std::placeholders::_1));
     }
+
+    void minecraft_proxy_connection::handle_connect(const asio::error_code& ec) {
+        if (!ec) {
+            start_transmission();
+        }
+    }
+
+    void minecraft_proxy_connection::start_transmission() {
+        
+    }
+
 }
